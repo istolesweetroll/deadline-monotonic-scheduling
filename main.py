894af4261090d615de899fc1634835a7487294be
istolesweetroll <77072731+istolesweetroll@users.noreply.github.com>
2021-11-08 11:05:21 +0100

    Error handling

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from PIL import Image
-from PyQt6 import uic, QtTest
-from PyQt6.QtGui import QImage, QPixmap, QPalette
-from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QScrollArea
-
+from PyQt6 import uic
+from PyQt6.QtGui import QImage, QPixmap
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel
+import time
 
 class UI(QWidget):
     def __init__(self):
         super().__init__()
         # this is used for loading ui file
         uic.loadUi("untitled.ui", self)
-        self.read_data()
+        f = open('data.txt', 'r')
+
+        with f:
+            data = f.read()
+            self.textEdit.setText(data)
+
         self.welcomeBackground.setStyleSheet("background-image:url(./background.png) ")
         self.resultsBackground.setStyleSheet("background-image:url(./background.png)")
         self.reset.setVisible(False)
@@ -23,35 +27,45 @@ class UI(QWidget):
     def switch_to_welcome(self):
         self.welcome.setVisible(True)
 
-
     def switch_to_results(self):
-        self.welcome.setVisible(False)
+        self.error.setVisible(False)
         with open('data.txt', 'w') as file:
             file.write(self.textEdit.toPlainText())
+        if self.isInputGood():
+            self.welcome.setVisible(False)
+            dms = Dms()
+            results = dms.run()
+            dms.save(results)
+            imageLabel = QLabel()
+            image = QImage("./gantt1.png")
+            self.reset.setVisible(True)
+            imageLabel.setPixmap(QPixmap.fromImage(image))
+            self.scrollArea.setWidget(imageLabel)
 
+        if not self.isInputGood():
+            self.error.setVisible(True)
 
-        dms = Dms()
-        results = dms.run()
-        dms.save(results)
-
-
-        imageLabel = QLabel()
-        image = QImage("./gantt1.png")
-        self.reset.setVisible(True)
-        imageLabel.setPixmap(QPixmap.fromImage(image))
-        self.scrollArea.setWidget(imageLabel)
-
-    def count_words(line):
-        words = line.split()
-        return len(words)
-
-    def read_data(self):
-        f = open('data.txt', 'r')
-
-        with f:
-            data = f.read()
-            self.textEdit.setText(data)
-
+    def isInputGood(self):
+        f = open("./data.txt",
+                 "r")
+        text = f.read()
+        lines = text.split('\n')
+        if len(lines) > 9:
+            return False
+        for line in lines:
+            words = line.split(' ')
+            length = len(line.split())
+            if not all(c.isdecimal() for c in words[1:]) or line[0] != "t":
+                f.close()
+                return False
+            if length != 4:
+                f.close()
+                return False
+            if not words[1] >= words[3] > words[2]:
+                f.close()
+                return False
+        f.close()
+        return True
 
 class Task:
     def __init__(self, task_name, period, execution_time, deadline):
@@ -62,10 +76,8 @@ class Task:
 
 
 class Dms:
-
     def read_data(self, filename: str):
         tasks = []
-
         with open(filename) as f:
             lines = [line.rstrip() for line in f]
             for x in lines:
@@ -76,26 +88,15 @@ class Dms:
 
         return tasks
 
-    @staticmethod
-    def validate_task(task: Task):
-        if task.period >= task.deadline > task.execution_time:
-            return True
-        else:
-            return False
-
     def run(self):
-        # def run(self, tasks):
         tasks = self.read_data("data.txt")
-
 
         lowest_common_multiple = np.lcm.reduce([x.period for x in tasks])
         result = []
         itr = 0
         completed_tasks = []
         tasks.sort(key=lambda x: x.deadline, reverse=False)
-        for task in tasks:
-            if not self.validate_task(task):
-                return result
+
         for x in range(0, lowest_common_multiple):
             for task in tasks:
                 if x % task.period == 0 and completed_tasks.__contains__(task) and not (x == 0):
@@ -130,7 +131,7 @@ class Dms:
         gnt.set_ylabel('zadania')
 
         yticklabels = []
-        facecolors = ['purple', 'red', 'blue', 'green', 'orange', 'yellow', 'pink', 'gray']
+        facecolors = ['purple', 'red', 'blue', 'green', 'orange', 'yellow', 'pink', 'gray', 'black']
         for x in args[1]:
             if x[2] not in yticklabels:
                 yticklabels.append(x[2])
@@ -149,6 +150,7 @@ class Dms:
                                 facecolors=facecolors[x[2] - 1])
 
         plt.xlim([0, xmax])
+        plt.ylim([1, len(yticklabels)+1])
         plt.savefig("gantt1.png", bbox_inches='tight');
 
 
